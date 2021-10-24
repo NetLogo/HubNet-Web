787b4bde167d27d146eb27a4fe46e5097c2af04b
TheBizzle <jason.bertsche@gmail.com>
2021-10-24 13:59:42 -0500

    Optimization: Only send zipped msg if it's smaller

--- js/protobuf/converters-common.js
+++ js/protobuf/converters-common.js
@@ -8,6 +8,8 @@ import * as FromJoinerJP from "./from-joiner-jiggery-pokery.js"
 import * as FromHostFurler   from "./from-host-furling.js"
 import * as FromJoinerFurler from "./from-joiner-furling.js"
 
+const cMask = 0b10000000;
+
 const trace = (type) => (f) => {
   const debugBlacklist = ["ping", "ping-result", "pong", "keep-alive"];
   if (!debugBlacklist.includes(type)) {
@@ -32,21 +34,39 @@ const encodePBuf = (isHost) => (msg) => {
     alert(fullError);
   }
 
-  const writer = protobuf.Writer.create();
-  writer.uint32(typeCode);
-
+  const writer     = protobuf.Writer.create();
   const encoded    = protoType.encode(protoMsg, writer).finish();
   const compressed = deflate(encoded);
 
-  return compressed;
+  const mask = (x) => x | cMask;
+
+  const [maskedCode, buffer] =
+    (compressed.byteLength < encoded.byteLength) ? [mask(typeCode), compressed] :
+                                                   [     typeCode , encoded   ];
+
+  const outBuffer = new Uint8Array(buffer.length + 1);
+  outBuffer[0]    = maskedCode;
+  outBuffer.set(buffer, 1);
+
+  return outBuffer;
 
 };
 
-const decodePBuf = (isHost) => (compressedMsg) => {
-  const furler            = isHost ? FromJoinerFurler : FromHostFurler
-  const msgBuf            = inflate(compressedMsg);
-  const [type, protoType] = furler.lookupTypeCode(msgBuf[0]);
-  const decoded           = protoType.decode(msgBuf.slice(1));
+const decodePBuf = (isHost) => (byteStream) => {
+
+  const furler = isHost ? FromJoinerFurler : FromHostFurler;
+
+  const initialCode = byteStream[0];
+  const dataStream  = byteStream.slice(1);
+
+  const unmask = (x) => x & 0b01111111;
+
+  const [typeCode, msgBuf] =
+    ((initialCode & cMask) === cMask) ? [unmask(initialCode), inflate(dataStream)] :
+                                        [       initialCode ,         dataStream ];
+
+  const [type, protoType] = furler.lookupTypeCode(typeCode);
+  const decoded           = protoType.decode(msgBuf);
   const decodedObj        = protoType.toObject(decoded, { enums: String });
   const reconstructed     = { type, ...decodedObj };
   const furled            = furler.furl(reconstructed);
@@ -55,6 +75,7 @@ const decodePBuf = (isHost) => (compressedMsg) => {
   trace(type)(() => console.log("Done to recombobulate", recombobulated));
 
   return recombobulated;
+
 };
 
 export { decodePBuf, encodePBuf }
