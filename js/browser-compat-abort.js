0c38685df2c8c6f71dbb3edf6dc960996a8815ee
TheBizzle <jason.bertsche@gmail.com>
2021-12-04 16:27:18 -0600

    Refactor: Clean up browser compat. blocker code

---
+++ js/browser-compat-abort.js
@@ -0,0 +1,13 @@
+const firefoxBad = `<p>We're sorry, but Firefox is not currently supported.
+  We plan to add support for Firefox once its "module Worker" functionality
+  has been implemented.  Updates on Mozilla's progress can be found on
+  <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=1247687">Firefox issue
+  #1247687</a>.</p>`;
+
+const useTheseInstead = "<p>In the meantime: Chromium, Brave, Chrome, Opera, and Edge can all be used to access HubNet Web.</p>";
+
+const checkUA = (name) => navigator.userAgent.includes(name);
+
+if (checkUA("Firefox")) {
+  document.body.innerHTML = `${firefoxBad}\n\n${useTheseInstead}`;
+}
--- js/firefox-abort.js
+++
@@ -1,9 +0,0 @@
-if (navigator.userAgent.includes("Firefox")) {
-  document.body.innerHTML = `<p>We're sorry, but Firefox is not currently
-  supported.  We plan to add support for Firefox once its "module Worker"
-  functionality has been implemented.  Updates on Mozilla's progress can be found on
-  <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=1247687">Firefox issue
-  #1247687</a>.</p>
-
-  <p>In the meantime, all other major browsers can be used to access HubNet Web.</p>`;
-}
