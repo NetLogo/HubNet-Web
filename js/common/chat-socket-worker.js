affaa45ed19c6f1070a19b15202771007165d11c
TheBizzle <jason.bertsche@gmail.com>
2022-01-28 11:08:38 -0600

    Prefactor: Centralize location of `genUUID`

--- js/common/chat-socket-worker.js
+++ js/common/chat-socket-worker.js
@@ -1,22 +1,7 @@
+import { genUUID }      from "./util.js";
 import WebSocketManager from "./websocket.js";
 
 import { hnw, wsProto } from "/js/static/domain.js";
-
-// () => UUID
-/* eslint-disable no-bitwise */
-const genUUID = () => {
-
-  const replacer =
-    (c) => {
-      const r = Math.random() * 16 | 0;
-      const v = c === "x" ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    };
-
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, replacer);
-
-};
-/* eslint-enable no-bitwise */
 
 const uuid = genUUID(); // UUID
 
--- js/common/util.js
+++ js/common/util.js
@@ -18,6 +18,22 @@ const checkIsTURN = (stats) => {
   );
 };
 
+// () => UUID
+/* eslint-disable no-bitwise */
+const genUUID = () => {
+
+  const replacer =
+    (c) => {
+      const r = Math.random() * 16 | 0;
+      const v = c === "x" ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    };
+
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, replacer);
+
+};
+/* eslint-enable no-bitwise */
+
 // (String) => Number
 /* eslint-disable no-bitwise */
 const uuidToRTCID = (uuid) => {
@@ -40,4 +56,4 @@ const typeIsOOB = (type) => {
   return ["bye-bye", "keep-alive", "ping", "pong"].includes(type);
 };
 
-export { byteSizeLabel, checkIsTURN, typeIsOOB, uuidToRTCID };
+export { byteSizeLabel, checkIsTURN, genUUID, typeIsOOB, uuidToRTCID };
