4cebf99bb3b685f461a27f27553facde367ceefc
TheBizzle <jason.bertsche@gmail.com>
2022-01-28 11:21:22 -0600

    Prefactor: Provide generalized UUID serializer funcs

--- js/serialize/jigpoke/common-jiggery-pokery.js
+++ js/serialize/jigpoke/common-jiggery-pokery.js
@@ -5,11 +5,11 @@ const transform = (obj) => (key, f) => {
   }
 };
 
-// (String, Object[Any]) => Unit
-const rejiggerToken = (token, parent) => {
+// (UUID) => (Number, Number, Number, Number)
+const rejiggerUUID = (uuid) => {
 
   const uuidRegex = /(........)-(....)-(4...)-(....)-(....)(........)/;
-  const [ , a, b, c, d, e, f] = token.match(uuidRegex);
+  const [ , a, b, c, d, e, f] = uuid.match(uuidRegex);
 
   // Must be broken into at least 3 parts.  There are 32 digits
   // in the hex number, and attempts to run `parseInt` on 16-digit
@@ -25,6 +25,15 @@ const rejiggerToken = (token, parent) => {
   const chunk3 = parseInt(`${d}${e}`, 16);
   const chunk4 = parseInt(f         , 16);
 
+  return [chunk1, chunk2, chunk3, chunk4];
+
+};
+
+// (String, Object[Any]) => Unit
+const rejiggerToken = (token, parent) => {
+
+  const [chunk1, chunk2, chunk3, chunk4] = rejiggerUUID(token);
+
   parent.tokenChunk1 = chunk1;
   parent.tokenChunk2 = chunk2;
   parent.tokenChunk3 = chunk3;
@@ -32,8 +41,8 @@ const rejiggerToken = (token, parent) => {
 
 };
 
-// (String, Object[Any]) => Unit
-const recombobulateToken = (target, parent) => {
+// (Number, Number, Number, Number) => UUID
+const recombobulateUUID = (part1, part2, part3, part4) => {
 
   const toHex = (numDigits) => (x) => {
     return x.toString(16).padStart(numDigits, "0");
@@ -41,10 +50,10 @@ const recombobulateToken = (target, parent) => {
 
   const toHex8 = toHex(8);
 
-  const chunk1 = toHex8(target.tokenChunk1);
-  const chunk2 = toHex8(target.tokenChunk2);
-  const chunk3 = toHex8(target.tokenChunk3);
-  const chunk4 = toHex8(target.tokenChunk4);
+  const chunk1 = toHex8(part1);
+  const chunk2 = toHex8(part2);
+  const chunk3 = toHex8(part3);
+  const chunk4 = toHex8(part4);
 
   const uuid1Regex = /(........)/;
   const uuid2Regex = /(....)(4...)/;
@@ -56,7 +65,18 @@ const recombobulateToken = (target, parent) => {
   const [ , d, e] = chunk3.match(uuid3Regex);
   const [ , f   ] = chunk4.match(uuid4Regex);
 
-  parent.token = `${a}-${b}-${c}-${d}-${e}${f}`;
+  return `${a}-${b}-${c}-${d}-${e}${f}`;
+
+};
+
+// (String, Object[Any]) => Unit
+const recombobulateToken = (target, parent) => {
+
+  const token =
+    recombobulateUUID( target.tokenChunk1, target.tokenChunk2
+                     , target.tokenChunk3, target.tokenChunk4);
+
+  parent.token = token;
 
 };
 
@@ -118,4 +138,5 @@ const deepClone = (x, blacklist = {}, shouldLowerCase = false, whitelist = {}) =
   return (typeof(x) !== "object") ? x : innerClone(x, blacklist, shouldLowerCase, whitelist);
 };
 
-export { deepClone, recombobulateToken, rejiggerToken, transform };
+export { deepClone, recombobulateToken, rejiggerToken
+       , recombobulateUUID, rejiggerUUID, transform };
