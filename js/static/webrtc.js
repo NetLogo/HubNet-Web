891b57c340e0d209f2de826420979bfda27f90e9
TheBizzle <jason.bertsche@gmail.com>
2022-02-04 11:15:17 -0600

    WIP: Remove ICE servers

--- js/static/webrtc.js
+++ js/static/webrtc.js
@@ -3,7 +3,7 @@ import deepFreeze from "/js/static/deep-freeze.js";
 // type Protocol = { connection :: RTCPeerConnection, channel :: RTCDataChannel, socket :: WebSocket }
 // type Channel  = RTCDataChannel
 
-const commonConfig = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
+const commonConfig = { iceServers: [{}] };
 
 deepFreeze(commonConfig);
 
