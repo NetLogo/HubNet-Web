fe08ea1c407596c4e748f0ee2f9b24c9769feed4
Christopher Song <chris4728.song2001@gmail.com>
2022-02-25 16:27:20 -0600

    Refactor: Move JS code into '/js/host/main.js' file

--- js/host/main.js
+++ js/host/main.js
@@ -99,27 +99,171 @@ const nlwManager =
                 , connMan.narrowcast, onNLWManError);
 
 document.addEventListener("DOMContentLoaded", () => {
-  const modalContainer = byEID("modal-container");
 
-  window.onclick = (event) => {
-    if (event.target === modalContainer) {
-      modalContainer.classList.add("hidden");
+  // (NEW): TODO
+  document.onclick = (event) => {
+    const onHostAPage = byEID("nlw-frame").classList.contains("hidden");
+
+    if (onHostAPage) {
+      if (event.target === descriptionModalContainer) {
+        descriptionModalContainer.classList.add("modal-invis");
+      }
+    } else {
+      if (event.target === statsModalContainer) {
+        hideModal(statsModalContainer);
+      } else if (event.target === codeModalContainer) {
+        hideModal(codeModalContainer);
+      } else if (event.target === modelInfoModalContainer) {
+        hideModal(modelInfoModalContainer);
+      }
     }
-  };
-
-  byEID("read-more-button").onclick = () => {
-    modalContainer.classList.remove("hidden");
-  };
-
-  byEID("close-modal-button").onclick = () => {
-    modalContainer.classList.add("hidden");
   };
 
   document.addEventListener("keydown", (event) => {
-    if (!modalContainer.classList.contains("hidden") && event.key === "Escape") {
-      modalContainer.classList.add("hidden");
+    const onHostAPage = byEID("nlw-frame").classList.contains("hidden");
+
+    if (onHostAPage) {
+      if (!descriptionModalContainer.classList.contains("modal-invis") && event.key === "Escape") {
+        descriptionModalContainer.classList.add("modal-invis");
+      }
+    } else {
+      if (event.key === "Escape") {
+        hideAllHostBModals();
+      }
     }
   });
+
+  // (NEW): TODO
+  const descriptionModalContainer = byEID("description-modal-container");
+
+  byEID("read-more-button").onclick = () => {
+    descriptionModalContainer.classList.remove("modal-invis");
+  };
+
+  byEID("close-description-modal-button").onclick = () => {
+    descriptionModalContainer.classList.add("modal-invis");
+  };
+
+  // (NEW): TODO
+  const capacityDisplayText = document.getElementById("capacity-display-text");
+  const capacitySlider = document.getElementById("max-num-clients-picker");
+
+  capacitySlider.onchange = (e) => {
+    capacityDisplayText.innerHTML = `Session Capacity: ${e.target.value}`;
+  };
+
+  const sessionClosedChatBox = document.getElementById("session-chat-box-closed");
+  const sessionClosedChatBoxBottom = document.getElementById("session-chat-box-closed-bottom");
+  const sessionOpenChatBox = document.getElementById("session-chat-box-open");
+  const sessionOpenChatHeader = document.getElementById("session-open-chat-header");
+
+  sessionClosedChatBox.onclick = () => {
+    sessionOpenChatBox.classList.remove("invisible");
+    sessionClosedChatBox.classList.add("invisible");
+    sessionClosedChatBoxBottom.classList.add("invisible");
+  };
+
+  sessionOpenChatHeader.onclick = () => {
+    sessionOpenChatBox.classList.add("invisible");
+    sessionClosedChatBox.classList.remove("invisible");
+    sessionClosedChatBoxBottom.classList.remove("invisible");
+  };
+
+  const globalClosedChatBox = document.getElementById("global-chat-box-closed");
+  const globalClosedChatBoxBottom = document.getElementById("global-chat-box-closed-bottom");
+  const globalOpenChatBox = document.getElementById("global-chat-box-open");
+  const globalOpenChatHeader = document.getElementById("global-open-chat-header");
+
+  globalClosedChatBox.onclick = () => {
+    globalOpenChatBox.classList.remove("invisible");
+    globalClosedChatBox.classList.add("invisible");
+    globalClosedChatBoxBottom.classList.add("invisible");
+  };
+
+  globalOpenChatHeader.onclick = () => {
+    globalOpenChatBox.classList.add("invisible");
+    globalClosedChatBox.classList.remove("invisible");
+    globalClosedChatBoxBottom.classList.remove("invisible");
+  };
+
+  const commandCenterClosedBox = document.getElementById("command-center-closed");
+  const commandCenterOpenBox = document.getElementById("command-center-open");
+  const commandCenterOpenHeader = document.getElementById("command-center-header");
+
+  commandCenterClosedBox.onclick = () => {
+    commandCenterOpenBox.classList.remove("invisible");
+    commandCenterClosedBox.classList.add("invisible");
+  };
+
+  commandCenterOpenHeader.onclick = () => {
+    commandCenterOpenBox.classList.add("invisible");
+    commandCenterClosedBox.classList.remove("invisible");
+  };
+
+  // (NEW): TODO
+  const statsModalTab = document.getElementById("stats-modal-tab");
+  const codeModalTab = document.getElementById("code-modal-tab");
+  const modelInfoModalTab = document.getElementById("model-info-modal-tab");
+
+  const closeStatsModalButton = document.getElementById("close-stats-modal-button");
+  const closeCodeModalButton = document.getElementById("close-code-modal-button");
+  const closeModelInfoModalButton = document.getElementById("close-model-info-modal-button");
+
+  const statsModalContainer = document.getElementById("stats-modal-container");
+  const codeModalContainer = document.getElementById("code-modal-container");
+  const modelInfoModalContainer = document.getElementById("model-info-modal-container");
+  const hostBPageMain = document.getElementById("nlw-frame");
+
+  const showModal = (modalContainer) => {
+    modalContainer.classList.remove("modal-invis");
+    hostBPageMain.classList.add("no-select");
+  };
+
+  const hideModal = (modalContainer) => {
+    modalContainer.classList.add("modal-invis");
+    hostBPageMain.classList.remove("no-select");
+  };
+
+  const hideAllHostBModals = () => {
+    const statsModalIsInvis = statsModalContainer.classList.contains("modal-invis");
+    const codeModalIsInvis = codeModalContainer.classList.contains("modal-invis");
+    const modelInfoModalIsInvis = modelInfoModalContainer.classList.contains("modal-invis");
+
+    if (!statsModalIsInvis) {
+      hideModal(statsModalContainer);
+    } else if (!codeModalIsInvis) {
+      hideModal(codeModalContainer);
+    } else if (!modelInfoModalIsInvis) {
+      hideModal(modelInfoModalContainer);
+    }
+  };
+
+  statsModalTab.onclick = () => {
+    hideAllHostBModals();
+    showModal(statsModalContainer);
+  };
+
+  codeModalTab.onclick = () => {
+    hideAllHostBModals();
+    showModal(codeModalContainer);
+  };
+
+  modelInfoModalTab.onclick = () => {
+    hideAllHostBModals();
+    showModal(modelInfoModalContainer);
+  };
+
+  closeStatsModalButton.onclick = () => {
+    hideModal(statsModalContainer);
+  };
+
+  closeCodeModalButton.onclick = () => {
+    hideModal(codeModalContainer);
+  };
+
+  closeModelInfoModalButton.onclick = () => {
+    hideModal(modelInfoModalContainer);
+  };
 
   nlwManager.init();
 });
