5560e7b4203be14926526b65916759dc1b52fc0d
TheBizzle <jason.bertsche@gmail.com>
2023-07-24 10:33:38 -0500

    Test: Add Cypress UI tests

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,28 @@
+describe("basic", () => {
+
+  it("connects with low latency", () => {
+
+    //cy.visit("https://hubnetweb.org/join")
+    cy.visit("http://localhost:8080/join")
+    cy.get("[data-cy='session-row']").click()
+    cy.get("[data-cy='username']").type("apples")
+    cy.get("[data-cy='submit']").click()
+    cy.get("[data-cy='view-details']").click()
+
+    cy.get("[data-cy='latency']", { timeout: 30000 }).should(
+      (span) => {
+        let latency = parseInt(span.text(), 10)
+        expect(latency).to.be.at.least(0)
+        expect(latency).to.be.at.most(200)
+      }
+    )
+
+    cy.screenshot()
+
+    cy.on('window:alert', (x) => {
+       expect(x).to.contains('No error occurred here.  None at all!');
+    })
+
+  })
+
+})
