3cff4f2624fee53f8b1a5148b5fc9ff96f0ee45b
TheBizzle <jason.bertsche@gmail.com>
2023-07-30 17:11:24 -0500

    Test: Have Cypress click buttons in model

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+  chromeWebSecurity: false
+, e2e: {
     setupNodeEvents(on, config) {
     },
   },
--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,11 +4,44 @@ describe("basic", () => {
 
     let idNum = Math.floor(Math.random() * 1e6)
 
+    // OPEN PAGE AND LOG IN
+
     //cy.visit("https://hubnetweb.org/join")
     cy.visit("http://localhost:8080/join")
     cy.get("[data-cy='session-row']").click()
     cy.get("[data-cy='username']").type(`apples-${idNum}`)
     cy.get("[data-cy='submit']").click()
+
+    // CLICK AROUND ON BUTTONS (assuming Disease model)
+
+    let frame = cy.get("iframe[data-cy='nlw-frame']")
+
+    frame.then(
+      (iframe) => {
+
+        let frameBody = cy.wrap(iframe.contents().find("body"))
+        frameBody.within(() => {
+
+          let upBtn    = cy.get("#netlogo-hnwButton-6")
+          let downBtn  = cy.get("#netlogo-hnwButton-7")
+          let leftBtn  = cy.get("#netlogo-hnwButton-8")
+          let rightBtn = cy.get("#netlogo-hnwButton-9")
+
+          let buttons = [upBtn, downBtn, leftBtn, rightBtn]
+
+          for (var i = 0; i < 120; i++) {
+            let button = buttons[Math.floor(Math.random() * buttons.length)]
+            button.click()
+            cy.wait(100)
+          }
+
+        })
+
+      }
+    )
+
+    // GET LATENCY STATS
+
     cy.get("[data-cy='view-details']").click()
 
     cy.get("[data-cy='latency']", { timeout: 30000 }).should(
@@ -18,6 +51,8 @@ describe("basic", () => {
         expect(latency).to.be.at.most(200)
       }
     )
+
+    // MISC
 
     cy.screenshot()
 
